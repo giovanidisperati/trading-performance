bbd80e94ebd23479a483aa9651ef80a18f823e69
Giovani Fonseca Ravagnani Disperati <giovanidisperati@gmail.com>
2020-03-03 22:00:21 -0300

    First commit.

---
+++ src/main/java/com/tradingperformance/tradingperformance/TradingPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.tradingperformance.tradingperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradingperformance/tradingperformance/TradingPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradingperformance.tradingperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
